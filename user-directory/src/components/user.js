8a2868d0a82570ae8b490201c4eb27d69d32cf6b
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-10-25 16:05:00 -0700

    added sample data

--- user-directory/src/components/user.js
+++ user-directory/src/components/user.js
@@ -3,10 +3,27 @@ import React, { Component } from "react";
 class User extends Component {
     render() {
         return (
-            <div>
-                <form>
-                    <h1>User directory</h1>
-                </form>
+            <div className="container">
+            <h1>Sample of users for now</h1>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Users</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>DOB</th>
+                        </tr>
+                        <tr>
+                            <td>Picture</td>
+                            <td>Soliana</td>
+                            <td>Robeil</td>
+                            <td>Solishikor1@gmail.com</td>
+                            <td>03/06/2019</td>
+
+                        </tr>
+                    </thead>
+                </table>
             </div>
         )
     }
