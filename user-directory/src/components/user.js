618786aae4a432328d864643abf8fb025b1532d3
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-10-25 15:42:47 -0700

    created user.js file

---
+++ user-directory/src/components/user.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class User extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <h1>User directory</h1>
+                </form>
+            </div>
+        )
+    }
+}
+export default User;
---
+++ user-directory/src/utils/API.js
@@ -0,0 +1,8 @@
+// import axios from 'axios';
+
+// // Gets all users
+// export default {
+//   getUsers: function() {
+//     return axios.get('https://randomuser.me/api/?results=200&nat=us');
+//   }
+// };
